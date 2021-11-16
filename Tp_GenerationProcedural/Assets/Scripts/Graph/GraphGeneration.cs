054442442da7882cf43b7ca4f9476e1f561c18a7
François <dessartsfran@gmail.com>
2021-11-16 11:09:07 +0100

    Secondary paths

--- Tp_GenerationProcedural/Assets/Scripts/Graph/GraphGeneration.cs
+++ Tp_GenerationProcedural/Assets/Scripts/Graph/GraphGeneration.cs
@@ -13,6 +13,7 @@ public class GraphGeneration : MonoBehaviour
         CreateStartNode();
         CreateDungeon(10);
         FinishDungeon();
+        SecondaryPath(5);
 
         foreach (var node in nodes)
             Debug.LogError($"Node pos : {node.pos}");
@@ -106,6 +107,108 @@ public class GraphGeneration : MonoBehaviour
         endNode.difficulty = 0;
 
         nodes.Add(endNode);
+    }
+
+    void SecondaryPath(int nodesnumber)
+    {
+        Nodes startingNode;
+        Nodes checkedNode;
+
+        bool north = false;
+        bool south = false;
+        bool east = false;
+        bool west = false;
+
+        do{
+
+            north = false;
+            south = false;
+            east = false;
+            west = false;
+
+            checkedNode = nodes[Random.Range(0, nodes.Count - 1)];
+
+            foreach (var _node in nodes)
+            {
+
+                if (_node.pos == checkedNode.pos + Utils.OrientationToDir(Utils.ORIENTATION.NORTH))
+                {
+                    north = true;
+                }
+                if (_node.pos == checkedNode.pos + Utils.OrientationToDir(Utils.ORIENTATION.SOUTH))
+                {
+                    south = true;
+                }
+                if (_node.pos == checkedNode.pos + Utils.OrientationToDir(Utils.ORIENTATION.EAST))
+                {
+                    east = true;
+                }
+                if (_node.pos == checkedNode.pos + Utils.OrientationToDir(Utils.ORIENTATION.WEST))
+                {
+                    west = true;
+                }
+            }
+        }
+        while (north && south && east && west);
+
+        startingNode = checkedNode;
+
+        for (int i = 0; i < nodesnumber; i++)
+        {
+            bool canContinue = true;
+            Vector2 previousNodePos;
+
+            do
+            {
+                if(i == 0) previousNodePos = startingNode.pos;
+                else previousNodePos = nodes[nodes.Count - 1].pos;
+                previousNodePos += Utils.OrientationToDir(GetOrientation());
+
+                foreach (var _node in nodes)
+                {
+                    if (_node.pos == previousNodePos)
+                    {
+                        canContinue = false;
+                        break;
+                    }
+                    else
+                        canContinue = true;
+                }
+
+            } while (canContinue == false);
+
+            Nodes node = new Nodes();
+
+            switch (GetOrientation())
+            {
+                case Utils.ORIENTATION.NORTH:
+                    node._orientation = Nodes.orientation.NORTH;
+                    break;
+                case Utils.ORIENTATION.EAST:
+                    node._orientation = Nodes.orientation.EAST;
+                    break;
+                case Utils.ORIENTATION.SOUTH:
+                    node._orientation = Nodes.orientation.SOUTH;
+                    break;
+                case Utils.ORIENTATION.WEST:
+                    node._orientation = Nodes.orientation.WEST;
+                    break;
+
+                default:
+                    break;
+            }
+
+            node.pos = previousNodePos;
+            node._type = Nodes.type.normal;
+            node.difficulty = Random.Range(0, 4);
+
+            nodes.Add(node);
+
+            Connections connection = new Connections();
+            connection.hasLocked = Random.Range(0, 2) == 0 ? false : true;
+
+            connections.Add(connection);
+        }
     }
 
     Utils.ORIENTATION GetOrientation()
