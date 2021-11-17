c284819fdd0f694fa783c976f6bba45782cc4352
François <dessartsfran@gmail.com>
2021-11-17 10:22:12 +0100

    Symetrie wip

--- Tp_GenerationProcedural/Assets/Scripts/Graph/GraphGeneration.cs
+++ Tp_GenerationProcedural/Assets/Scripts/Graph/GraphGeneration.cs
@@ -15,7 +15,7 @@ public class GraphGeneration : MonoBehaviour
     {
         CreateStartNode();
         CreateDungeon(10);
-        SecondaryPath(5);
+        //SecondaryPath(5);
 
         InstanceRoom();
 
@@ -40,11 +40,12 @@ public class GraphGeneration : MonoBehaviour
         startConnections.previousNode = startNode;
 
         connections.Add(startConnections);
+        
     }
 
     void CreateDungeon(int nodesnumber)
     {
-        for(int i = 0; i < nodesnumber; i++)
+        for(int i = 0; i < nodesnumber/2; i++)
         {
             bool canContinue = true;
             Vector2 previousNodePos;
@@ -75,7 +76,64 @@ public class GraphGeneration : MonoBehaviour
 
             nodes.Add(node);
 
-            if (i == nodesnumber - 1)
+            node._type = Nodes.type.normal;
+            node.difficulty = Random.Range(0, 4);
+
+            Connections connection = new Connections();
+            connection.hasLocked = Random.Range(0, 2) == 0 ? false : true;
+
+            connection.previousNode = node;
+
+            connections.Add(connection);
+        }
+
+        List<Connections> rConnections = connections;
+        rConnections.Reverse();
+
+        for (int j = nodesnumber/2; j < nodesnumber; j++)
+        {
+            bool canContinue = true;
+            Vector2 previousNodePos;
+ 
+            do
+            {
+                if(j == nodesnumber / 2) {
+                    previousNodePos = nodes[nodes.Count - 1].pos;
+                    previousNodePos += Utils.OrientationToDir(GetOrientation());
+
+                    foreach (var _node in nodes)
+                    {
+                        if (_node.pos == previousNodePos)
+                        {
+                            canContinue = false;
+                            break;
+                        }
+                        else
+                            canContinue = true;
+                    }
+                }
+                else
+                {
+                    Vector2 symPos;
+                    symPos.x = rConnections[j - (nodesnumber / 2)].nextNode.pos.x - rConnections[j - (nodesnumber / 2)].previousNode.pos.x;
+                    symPos.y = rConnections[j - (nodesnumber / 2)].nextNode.pos.y - rConnections[j - (nodesnumber / 2)].previousNode.pos.y;
+
+                    previousNodePos = nodes[nodes.Count - 1].pos + symPos;
+
+                    canContinue = true;
+                }
+
+            } while (canContinue == false);
+
+            Nodes node = new Nodes();
+
+            node.pos = previousNodePos;
+
+            connections[connections.Count - 1].nextNode = node;
+
+            nodes.Add(node);
+
+            if (j == nodesnumber - 1)
             {
                 node._type = Nodes.type.end;
                 node.difficulty = 0;
@@ -92,8 +150,27 @@ public class GraphGeneration : MonoBehaviour
 
                 connections.Add(connection);
             }
-
         }
+
+        /*Nodes nodeEnd = new Nodes();
+
+        Vector2 previousNodePosEnd;
+
+        nodeEnd._type = Nodes.type.end;
+        nodeEnd.difficulty = 0;
+
+        Vector2 symPosEnd;
+
+        symPosEnd.x = connections[0].nextNode.pos.x - connections[0].previousNode.pos.x;
+        symPosEnd.y = connections[0].nextNode.pos.y - connections[0].previousNode.pos.y;
+
+        previousNodePosEnd = nodes[nodes.Count - 1].pos + symPosEnd;
+
+        nodeEnd.pos = previousNodePosEnd;
+
+        connections[connections.Count - 1].nextNode = nodeEnd;
+
+        nodes.Add(nodeEnd);*/
     }
 
     void SecondaryPath(int nodesnumber)
