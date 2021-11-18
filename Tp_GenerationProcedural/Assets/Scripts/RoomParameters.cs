4d1b634295bf54620138f6d5c3d84e68c6e713bf
François <dessartsfran@gmail.com>
2021-11-18 09:24:12 +0100

    roomParam class

---
+++ Tp_GenerationProcedural/Assets/Scripts/RoomParameters.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomParameters
+{
+    public int difficulty;
+    public bool hasKey;
+
+    public bool isEnd;
+    public bool isStart;
+}
